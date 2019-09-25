5a6db2f4c5524726fd6b828bf4385e475da7d6a6
abhiyush <abhiyushshrestha@gmail.com>
2019-09-26 00:33:00 +1000

    Added another class to handle query search

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ from dataLoader import DataLoader
 from sequentialSearch import SequentialSearch
 import time
 import argparse
+from queryHandler import QueryHandler
+from output import Output
 
 
 if __name__ == "__main__":
+
+    handler = QueryHandler()
+    o = Output()
+
     # construct the argument parse and parse the arguments
     ap = argparse.ArgumentParser()
     ap.add_argument("-p", "--points", type=str, required=True,
@@ -15,114 +21,22 @@ if __name__ == "__main__":
     ap.add_argument("-q", "--queries", type=str, required=True,
                     help="path to queries")
     args = vars(ap.parse_args())
-    loader = DataLoader()
-    r_tree = RTree()
-    sequential_search = SequentialSearch()
 
-    # Loading the given data points
-    print("\n\nLoading data points...")
-    start_time = time.time()
-    points = loader.load_datapoints(args['points'])
-    end_time = time.time()
-    print("Data points loaded successfully!!!")
+    points = handler.datapoints_loader(args['points'])
+    queries = handler.queries_loader(args['queries'])
 
+    handler.create_rtree_index(points)
+    queries_result_sequential = handler.sequential_search(queries, points)
+    queries_result_rtree = handler.rtree_search(queries)
 
-    # Loading all the queries
-    print("\n\nLoading queries...")
-    start_time = time.time()
-    queries = loader.load_query(args['queries'])
-    end_time = time.time()
-    print("Queries loaded successfully!!!")
-    print("Time taken for loading queries:", end_time - start_time)
-
-    # Creating a R-tree index
-    print("\n\nCreating index for r-tree...")
-    start_time = time.time()
-    for index, point in points.iterrows():
-        r_tree.insert(r_tree.root, point)
-    end_time = time.time()
-    print("Rtree index created successfully!!!")
-    print("Time taken for loading data points:", end_time - start_time)
-
-
-    # Sequential search
-    print("\n\nSequential search::Querying all queries...")
-    queries_result = []
-    start_time = time.time()
-    for index, query in queries.iterrows():
-        n = sequential_search.sequential_query(points, query)
-        queries_result.append(n)
-    end_time = time.time()
-    print("Sequential search::Query completed successfully!!!")
-    print("Time taken for query using sequential search:", end_time - start_time)
-    print("Search result for Sequential search (all queries)::", queries_result)
+    o.save_as_txt(queries_result_sequential, "queries_result_sequential.txt")
+    o.save_as_txt(queries_result_rtree, "queries_result_rtree.txt")
 
 
 
-    # Sequential search for each query
-    print("\n\nSequential search::Querying 1 query...")
-    start_time = time.time()
-    q = {
-        'x1': 17840,
-        'x2': 18840,
-        'y1': 13971,
-        'y2': 14971
-    }
-    n = sequential_search.sequential_query(points, q)
-    end_time = time.time()
-    print("Sequential search::Query completed successfully!!!")
-    print("Time taken for searching single query:", end_time - start_time)
-    print("query result for sequential search (for 1 query)::", n)
-
-
-    # Searching using R-tree
-    print("\n\nQuerying all queries using r_tree indexing...")
-    queries_result = []
-    start_time = time.time()
-    for index, query in queries.iterrows():
-        n = r_tree.query(r_tree.root, query)
-        queries_result.append(n)
-    end_time = time.time()
-    print("Rtree::Query completed successfully!!!")
-    print("Time taken for loading data points:", end_time - start_time)
-    print("query result for R-tree search (for all query)::", queries_result)
-
-    # Searching using R-tree for each query
-    print("\n\nQuerying 1 query using r_tree indexing...")
-    start_time = time.time()
-    n = r_tree.query(r_tree.root, q)
-    end_time = time.time()
-    print("Rtree::Query completed successfully!!!")
-    print("Time taken for loading data points:", end_time - start_time)
-    print("query result for R-tree search (for 1 query)::", n)
-
-    # print("\nquery result for 10 different queries:", queries_result)
-    # print("\ndata points::", r_tree.root.data_points)
-    # print("\nchild nodes::", r_tree.root.child_nodes)
-    # print("\nMBR::", r_tree.root.MBR)
-    # print("\nparent::", r_tree.root.parent)
 
 
 
-q = {
-    'x1': 17840,
-    'x2': 18840,
-    'y1': 13971,
-    'y2': 14971
-}
-
-q = {
-    'x1': 33451,
-    'x2': 34451,
-    'y1': 29693,
-    'y2': 30693
-}
-q = {
-    'x1': 7187,
-    'x2': 8187,
-    'y1': 56997,
-    'y2': 57997
-}
 
 
 
---
+++ queryHandler.py
@@ -0,0 +1,106 @@
+from rtree import RTree
+from dataLoader import DataLoader
+from sequentialSearch import SequentialSearch
+import time
+
+class QueryHandler():
+
+    def __init__(self):
+        self.loader = DataLoader()
+        self.r_tree = RTree()
+        self.sequential_search = SequentialSearch()
+
+    # Loading the given data points
+    def datapoints_loader(self, datapoints_path):
+        print("\n\nLoading data points...")
+        start_time = time.time()
+        points = self.loader.load_datapoints(datapoints_path)
+        end_time = time.time()
+        print("Data points loaded successfully!!!")
+        return points
+
+    # Loading all the queries
+    def queries_loader(self, queries_path):
+        print("\n\nLoading queries...")
+        start_time = time.time()
+        queries = self.loader.load_query(queries_path)
+        end_time = time.time()
+        print("Queries loaded successfully!!!")
+        print("Time taken for loading queries:", end_time - start_time)
+        return queries
+
+    # Creating a R-tree index
+    def create_rtree_index(self, points):
+        print("\n\nCreating index for r-tree. Please wait for a while...")
+        start_time = time.time()
+        for index, point in points.iterrows():
+            self.r_tree.insert(self.r_tree.root, point)
+        end_time = time.time()
+        print("Rtree index created successfully!!!")
+        print("Time taken for loading data points:", end_time - start_time)
+
+    # Sequential search
+    def sequential_search(self, points, queries, single = False):
+        print("\n\nSequential search:: Performing search. Please wait...")
+        queries_result_sequential = []
+        if single:
+            # Sequential search for each query
+            print("\n\nSequential search (only 1 query)::Performing search. Please wait...")
+            start_time = time.time()
+            q = {
+                'x1': 17840,
+                'x2': 18840,
+                'y1': 13971,
+                'y2': 14971
+            }
+            n = self.sequential_search.sequential_query(points, q)
+            end_time = time.time()
+            queries_result_sequential.append(n)
+            print("Sequential search::Query completed successfully!!!")
+            print("Time taken for searching single query:", end_time - start_time)
+            print("query result for sequential search (for 1 query)::", queries_result_sequential)
+
+        else:
+            start_time = time.time()
+            for index, query in queries.iterrows():
+                n = self.sequential_search.sequential_query(points, query)
+                queries_result_sequential.append(n)
+            end_time = time.time()
+            print("Sequential search::Query completed successfully!!!")
+            print("Time taken for sequential query: ", end_time - start_time)
+            print("Average time taken for sequential query: ", (end_time - start_time) / len(queries))
+            print("Search result for Sequential search (all queries)::", queries_result_sequential)
+
+
+
+    # Searching using R-trees
+    def rtree_search(self, queries, single = False):
+        print("\n\n R-tree:: Performing search. Please wait...")
+        queries_result_rtree = []
+        if single:
+            # Searching using R-tree for each query
+            print("\n\nR-tree (1 query) :: Performing search. Please wait...")
+            start_time = time.time()
+            q = {
+                'x1': 17840,
+                'x2': 18840,
+                'y1': 13971,
+                'y2': 14971
+            }
+            n = self.r_tree.query(self.r_tree.root, q)
+            end_time = time.time()
+            queries_result_rtree.append(n)
+            print("Rtree::Query completed successfully!!!")
+            print("Time taken for loading data points:", end_time - start_time)
+            print("query result for R-tree search (for 1 query)::", queries_result_rtree)
+
+        else:
+            start_time = time.time()
+            for index, query in queries.iterrows():
+                n = self.r_tree.query(self.r_tree.root, query)
+                queries_result_rtree.append(n)
+            end_time = time.time()
+            print("Rtree::Query completed successfully!!!")
+            print("Time taken for R-Tree query:", end_time - start_time)
+            print("Average time taken for R-Tree query: ", (end_time - start_time) / len(queries))
+            print("query result for R-tree search (for all query)::", queries_result_rtree)
