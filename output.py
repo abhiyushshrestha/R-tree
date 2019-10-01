e119b1c9b9e0e5dd806f914a7d120b3aab8269b0
abhiyush <abhiyushshrestha@gmail.com>
2019-10-02 02:17:45 +1000

    Refactored the code

--- output.py
+++
@@ -1,7 +0,0 @@
-class Output():
-
-    @staticmethod
-    def save_as_txt(results, filename = "query_result.txt"):
-        with open(filename, "w") as f:
-            for result in results:
-                f.write(str(result) + "\n")
